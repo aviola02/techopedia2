ec078336b607cc5bb7cfeb09b59335c4f06c96a1
hmicha01 <hmicha01@cs.ucy.ac.cy>
2016-02-14 12:55:03 +0200

    The Main Menu is created as a separate file in order to be included where is needed. For easy reuse.

---
+++ code/MainMenu.php
@@ -0,0 +1,38 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: hamdy
+ * Date: 2/14/16
+ * Time: 12:47 PM
+ */
+ !-->
+
+
+<div class="navbar navbar-fixed-top" >
+    <div class="navbar-inner" style="background-color:#2d2b32">
+        <div class="container" style="background-color:#2d2b32;">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">TechnoPedia</a>
+            <div class="nav-collapse collapse navbar-inverse-collapse" style="left: 0px; top: 0px">
+                <ul class="nav pull-left">
+                    <li><a href="#" style="color:white">Timetable</a></li>
+                    <li><a href="#" style="background-color:gray; color:white">Students</a></li>
+                    <li><a href="#" style="color:white">Staff</a></li>
+                    <li><a href="#" style="color:white">Schedule</a></li>
+                    <li><a href="#" style="color:white">Events</a></li>
+                    <li><a href="#" style="color:white">Attendances</a></li>
+                    <li><a href="#" style="color:white">Fees</a></li>
+                    <li><a href="#" style="color:white">Exams&Results</a></li>
+                    <li><a href="#" style="color:white">Expenses</a></li>
+
+
+
+
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- /navbar-inner -->
+</div>
--- code/admin_students_add.php
+++ code/admin_students_add.php
@@ -1,9 +1,9 @@
-/**
+<!--/**
  * Created by PhpStorm.
  * User: hamdy
  * Date: 2/13/16
  * Time: 8:33 PM
- */
+ */!-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,34 +20,9 @@
               rel='stylesheet'>
     </head>
 <body>
-<div class="navbar navbar-fixed-top" >
-    <div class="navbar-inner" style="background-color:#2d2b32">
-        <div class="container" style="background-color:#2d2b32;">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">TechnoPedia</a>
-            <div class="nav-collapse collapse navbar-inverse-collapse" style="left: 0px; top: 0px">
-                <ul class="nav pull-left">
-                    <li><a href="#" style="color:white">Timetable</a></li>
-                    <li><a href="#" style="background-color:gray; color:white">Students</a></li>
-                    <li><a href="#" style="color:white">Staff</a></li>
-                    <li><a href="#" style="color:white">Schedule</a></li>
-                    <li><a href="#" style="color:white">Events</a></li>
-                    <li><a href="#" style="color:white">Attendances</a></li>
-                    <li><a href="#" style="color:white">Fees</a></li>
-                    <li><a href="#" style="color:white">Exams&Results</a></li>
-                    <li><a href="#" style="color:white">Expenses</a></li>
-
-
-
-
-                </ul>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- /navbar-inner -->
-</div>
+<?php
+include 'MainMenu.php';
+?>
 
 
 
