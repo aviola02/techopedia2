49d6e5afa5e63149e1bcae4bab30d396feb0b00f
hmicha01 <hmicha01@cs.ucy.ac.cy>
2016-02-22 23:02:22 +0200

    backend

---
+++ version2/classNav.php
@@ -0,0 +1,23 @@
+<!--
+  Created by PhpStorm.
+  User: hamdy
+  Date: 2/22/16
+  Time: 8:55 PM
+-->
+
+
+
+<?php
+include 'pullData2.php';
+
+$data = getData("Class");
+
+for ()
+
+
+
+
+?>
+
+<li><a href="icon.html"><i class="icon-angle-right"></i> Icon & Button</a></li>
+<li><a href="progress.html"><i class="icon-angle-right"></i> Progress</a></li>
--- version2/sample.php
+++ version2/sample.php
@@ -5,3 +5,6 @@
  * Date: 2/22/2016
  * Time: 3:04 PM
  */
+
+$sdaf = random_int(1,10);
+echo $sdaf;
